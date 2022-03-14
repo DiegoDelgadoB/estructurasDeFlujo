889a585fa0785faf36ad87e08fcb4493f1494789
Diego Delgado Barra <diego.delgadobarra@outlook.com>
2022-03-14 14:06:40 -0300

    funciones y Math

---
+++ assets/js/scriptFunciones.js
@@ -0,0 +1,15 @@
+// funciones, introducción
+// Generar un código que nos permita obtener un número aleatorio entre 0 y 10
+// desde el sistema, mostrarlo por consola
+function numeroAleatorio(){
+    var numeroAlAzar;
+    numeroAlAzar = Math.trunc(Math.random() * 10) + 1;
+    console.log(numeroAlAzar);
+}
+
+numeroAleatorio();
+
+// standar utilizado para asignar el nombre de las variables
+// lowerCamelCase = nombres de variables, objetos, funciones
+// UpperCamelCase = nombres de clases
+
